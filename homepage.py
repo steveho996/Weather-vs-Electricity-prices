64eaf3bd6bc8a1b3a46fa63e328fd91da9ba92eb
Duc Ho <92218817+steveho996@users.noreply.github.com>
2025-03-07 02:11:57 +0200

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -6,9 +6,9 @@ st.title("📊 Data Analysis Report")
 st.sidebar.success("Select a sidebar above 🖕")
 
 st.write("**Welcome to the data analysis dashboard. Use the sidebar to navigate between pages.**")
-st.write("""This Data Analysis Report base on company's data file **Electricity_20-09-2024.csv** which contains information about hourly electricity consumption (column Energy (kWh)), Temperature and the government electricity price file **sahkon-hinta-010121-240924.csv** which contains information about hourly electricity prices.
+st.write("""This Data Analysis Report base on data file **fmi_weather_and_price.csv** which contains information about time, weather conditions and eletricity prices.
          \nThis interactive data visualization dashboard provides insights into Finnish electricity consumption, pricing trends, and temperature variations over a selected period. By analyzing real-world electricity usage data, the dashboard helps users understand how energy consumption fluctuates and how pricing impacts overall electricity costs.""")
 
 
 # Display an image from a local file
-st.image("/Users/HoLuongDuc/Mac Air Data/Master Degree/Data and Math/Python/Elec.jpg", caption="Electricity Cost Management", use_container_width=True)
+st.image("/workspaces/Weather-vs-Electricity-prices/Electricity-Towers.jpeg", caption="Electricity Weather", use_container_width=True)
